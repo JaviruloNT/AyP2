985acf7b3bf3e989318b19dfa6917ef5bbe0a294
JaviruloNT <albapeque2015@gmail.com>
2025-04-10 23:04:15 -0300

    nuevos ejercicios tp2

---
+++ src/practicos/segundo/InvertidorVectores.java
@@ -0,0 +1,36 @@
+package practicos.segundo;
+
+import java.util.Arrays;
+
+public class InvertidorVectores {
+    public static String printArr(int[] arr) {
+        return Arrays.toString(arr);
+    }
+    public static int[] invertirVector(int[] arr) {
+        //
+        for (int i = 0; i < arr.length/2; i++) {
+            int j = arr.length - 1 - i;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        //
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] vA = {1,2,3,4,5};
+        int[] vB = {6,3,8,4};
+        int[] vC = {1};
+        int[] vD = {};
+        //
+        System.out.println(printArr(vA));
+        System.out.println(printArr(invertirVector(vA)));
+        System.out.println(printArr(vB));
+        System.out.println(printArr(invertirVector(vB)));
+        System.out.println(printArr(vC));
+        System.out.println(printArr(invertirVector(vC)));
+        System.out.println(printArr(vD));
+        System.out.println(printArr(invertirVector(vD)));
+    }
+}
