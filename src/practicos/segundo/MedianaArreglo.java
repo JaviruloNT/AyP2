250be680dbe23a52255599ffc128894974a89ed1
javirulo <albapeque2015@gmail.com>
2025-04-14 12:37:36 -0300

    ejercicio mediana

--- src/practicos/segundo/MedianaArreglo.java
+++ src/practicos/segundo/MedianaArreglo.java
@@ -1,13 +1,38 @@
 package practicos.segundo;
 
 public class MedianaArreglo {
-    public static int getMedian(int[] arr) {
-        int m = 0; // Mediana TODO: quitar 0
-        //
-        for (int n : arr) {
-
+    public static float getMedian(int[] arr) {
+        // Ordenar arr (Seleccion)
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
         //
-        return m;
+        int mediana = arr.length/2;
+        if (arr.length % 2 == 0) { // Arreglo Par
+            return (float)(arr[mediana-1]+arr[mediana])/2;
+        } else { // Arreglo Impar
+            return arr[mediana]; //
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arregloPar1 = {2,3};
+        int[] arregloImpar1 = {2,3,4};
+        int[] arregloImpar2 = {3,7,4};
+        int[] arregloPar2 = {5,7,9,3};
+        int[] arregloPar3 = {42, 17, 9, 55, 28, 12, 81, 3};
+        System.out.println(getMedian(arregloPar1)); // 2,5
+        System.out.println(getMedian(arregloImpar1)); // 3
+        System.out.println(getMedian(arregloImpar2)); // 4
+        System.out.println(getMedian(arregloPar2)); // 4
+        System.out.println(getMedian(arregloPar3)); //
     }
 }
